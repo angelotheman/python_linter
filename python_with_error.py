9e106b384125872fcee8acf77a4b91d355174b98
Kwabena Twumasi <ktwum721@gmail.com>
2024-05-21 16:52:49 +0000

    This is with an error

---
+++ python_with_error.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is a function file
+"""
+
+def hello():
+    print("hello World")
+
+def new():
+  print("This is me")
+
+if __name__ == '__main__':
+    hello()
+    new()
